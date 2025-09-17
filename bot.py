1af374a5d5c48d93de21575e5e6299edd089cd7f
svillors <tntzator9@gmail.com>
2025-09-17 08:11:57 +0200

    Add default settings to environment variables

--- bot.py
+++ bot.py
@@ -213,8 +213,8 @@ def get_database_connection():
     global _database
     if _database is None:
         database_password = os.getenv("DATABASE_PASSWORD")
-        database_host = os.getenv("DATABASE_HOST")
-        database_port = os.getenv("DATABASE_PORT")
+        database_host = os.getenv("DATABASE_HOST", '127.0.0.1')
+        database_port = os.getenv("DATABASE_PORT", '6379')
         _database = redis.Redis(host=database_host, port=database_port, password=database_password)
     return _database
 
